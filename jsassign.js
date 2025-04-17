323bdd210a9d504d0cce83b3f2c473dc94ef6e82
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-04-17 10:29:29 +0530

    js assignmenet

---
+++ jsassign.js
@@ -0,0 +1,15 @@
+function FormatCurrency(amount,currencyCode){
+    amount=amount.toFixed(2);
+    console.log(amount);
+    let amountString=String(amount);
+    let splitt=amountString.split('.');
+    console.log(splitt);
+    let realPart=String(splitt{0});
+    let decimalPart=splitt[1];
+    let stringLen=             
+    if(currencyCode==='INR'){
+
+    }
+   
+}
+FormatCurrency(12345,'INR')
